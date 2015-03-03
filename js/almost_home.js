7fc2f398f8b4dbeda2c49db0de345abcb035f9a1
Katelyn Lesse <katelyn@betterment.com>
2015-03-03 10:45:31 -0500

    working on first time creating db and key and secret sharing

--- js/almost_home.js
+++ js/almost_home.js
@@ -9,20 +9,48 @@
 //if yes then go home
 
 function didntExist() {
-    var shares = secrets.share(secrets.random(256), 2, 2);
+    var shares = secrets.share(secrets.random(256), t, 2);
     // put one share on dbox
-    client.writeFile("SCPS/share.txt", shares[0], function(error, stat) {
-        if (error) {
-            return console.log(error);
-        }
+    chrome.runtime.getBackgroundPage(function(eventPage) {
+        eventPage.controller.dbox_chrome.client.writeFile("SCPS/share.txt", shares[0], function(error, stat) {
+            if (error) {
+                return console.log(error);
+            }
+        });
     });
 
     // put one share on box
-    //shares[1]
+    var uploadUrl = 'https://upload.box.com/api/2.0/files/content';
+
+    // The Box OAuth 2 Header. Add your access token.
+    var headers = {
+        Authorization: localStorage.getItem("box_access_token")
+    };
+
+    $.ajax({
+        url: uploadUrl,
+        headers: headers,
+        type: 'POST',
+        // This prevents JQuery from trying to append the form as a querystring
+        processData: false,
+        contentType: false,
+        data: shares[1]
+    }).complete(function ( data ) {
+        // Log the JSON response to prove this worked
+        console.log(data.responseText);
+    });
 
 
-    // create empty pwdb, encrypt it, upload it
-    //var sql = window.SQL;
+    // create empty pwdb
+    var sql = window.SQL;
+    var pwdb = new sql.Database();
+    var sqlstr = "CREATE TABLE pws ()";
+    pwdb.run(sqlstr);
+    var binaryArray = pwdb.export();
+
+    //encrypt it, upload it
+    CryptoJS.AES.encrypt(binaryArray, secrets.combine(shares[0], shares[1]));
+    
 
 }
 
